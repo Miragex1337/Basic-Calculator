7913a442b7c0ca19d8e3a579a8d9d7e3560bc3b6
Alex <84569950+Miragex1337@users.noreply.github.com>
2022-04-19 22:28:24 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a, b, sum;
+
+    cout << "Enter a number: " << endl;
+    cin >> a;
+    cout << "Enter a number you like to add that number: " << endl;
+    cin >> b;
+
+    sum = a + b;
+
+    cout << "Final result is: " << " ";
+    cout << sum << endl;
+
+    return 0;
+}
